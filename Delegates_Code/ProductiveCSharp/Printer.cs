d8dd192e65c6fd2a7aa45e10e82e670c3437e811
clayman33 <33763757+clayman33@users.noreply.github.com>
2021-12-01 22:33:23 -0600

    master commit for folder name change

---
+++ Delegates_Code/ProductiveCSharp/Printer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProductiveCSharp
+{
+    public class Printer
+    {
+        public void PrettyPrint(int n)
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("  Result is " + n);
+            Console.WriteLine("---------------");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Delegates_Code/ProductiveCSharp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ProductiveCSharp
+{
+    public class Program
+    {
+        public delegate void Display(int n);
+
+        public delegate int Operation(int a, int b);
+
+        public static void Main()
+        {
+            Display display = new Printer().PrettyPrint;
+            Operation operation = Product;
+
+            Calculate(display, operation);
+        }
+
+        private static void Calculate(Display display, Operation operation)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+
+            var result = operation(a, b);
+
+            display(result);
+        }
+
+        private static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private static int Product(int a, int b)
+        {
+            return a * b;
+        }
+
+        private static void Print(int n)
+        {
+            Console.WriteLine("Result is " + n);
+        }
+    }
+}
