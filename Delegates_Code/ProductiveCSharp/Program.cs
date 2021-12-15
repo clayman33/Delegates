ca714df7e37fdabc1f9529fe2f319aca9879a99a
clayman33 <33763757+clayman33@users.noreply.github.com>
2021-12-14 23:11:56 -0600

    Modified to take user input and decrease the count.

--- Delegates_Code/ProductiveCSharp/Program.cs
+++ Delegates_Code/ProductiveCSharp/Program.cs
@@ -6,18 +6,28 @@ namespace ProductiveCSharp
     {
         public static void Main()
         {           
+            Console.WriteLine("How many children do you have: ");
+            var TotalChildren = int.Parse(Console.ReadLine());
 
-            Calculate(
-                n => Console.WriteLine("Result is " + n), 
-                (a, b) => a * b);
+            while (TotalChildren > 0)
+            {
+                    Calculate(
+                        age => Console.WriteLine("Your child is " + age),
+                        (yearBorn, curYear) => curYear - yearBorn);
+                    
+                TotalChildren--;
+            }
         }
 
         private static void Calculate(Action<int> display, Func<int, int, int> operation)
-        {
-            var a = int.Parse(Console.ReadLine());
-            var b = int.Parse(Console.ReadLine());
+        {       
+            Console.WriteLine("Enter the year the child was born: ");
+            var yearBorn = int.Parse(Console.ReadLine());
 
-            var result = operation(a, b);
+            Console.WriteLine("Enter the current year: ");
+            var curYear = int.Parse(Console.ReadLine());
+
+            var result = operation(yearBorn, curYear);
 
             display(result);
         }                   
